b8b0463dc6728fbde343e33da2d0f173daf662f4
Edward Lee <edward.lee@gmail.com>
2023-08-19 16:32:47 -0400

    Initial commit

---
+++ src/main/java/com/technodrome/rl/ReinforcementLearningApplication.java
@@ -0,0 +1,13 @@
+package com.technodrome.rl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinforcementLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReinforcementLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technodrome/rl/ReinforcementLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technodrome.rl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinforcementLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
